4f71e9d0f7dcc973e95d4584c4c5630ac0a4597d
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-10-09 14:54:04 +0300

    updatePersist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  addContact,
+  deleteContact,
+  updateFilter,
+} from '../redux/contactsSlice';
+import ContactList from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import SearchInput from './SearchInput/SearchInput';
+import { initializePersist } from '../redux/contactsSlice';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const addContactHandler = ({ name, number }) => {
+    const existingContact = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (existingContact) {
+      alert(`${name} is already in contacts!`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    dispatch(addContact(newContact));
+  };
+
+  const handleFilterChange = e => {
+    dispatch(updateFilter(e.target.value));
+  };
+
+  const deleteContactHandler = id => {
+    dispatch(deleteContact(id));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    dispatch(initializePersist());
+  }, [contacts, dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ marginLeft: '30px' }}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContactHandler} />
+      <h2>Contacts</h2>
+      <h3>Find contacts by name</h3>
+      <SearchInput value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        deleteContact={deleteContactHandler}
+      />
     </div>
   );
 };
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAction } from '@reduxjs/toolkit';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    updateFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addContact, deleteContact, updateFilter } =
+  contactsSlice.actions;
+export default contactsSlice.reducer;
+
+export const initializePersist = createAction('contacts/initializePersist');
